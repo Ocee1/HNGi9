1522689ddffc38ece2ae76b2e69ae8dda1d6c786
Ochapa James <ocjay24@gmail.com>
2022-10-28 05:18:36 +0100

    initial

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const PORT = 4000;
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get('/api', (req, res) => {
+    const data = {
+        "slackUsername": "Ocee",
+        "backend": true,
+        "age": 23,
+        "bio": "My name is James, I am a backend web developer. A graduate of Computer Science from Caritas University"
+    };
+    res.json(data);
+});
+
+app.listen(PORT, () => {console.log(`Server started on port: ${PORT}`)});
